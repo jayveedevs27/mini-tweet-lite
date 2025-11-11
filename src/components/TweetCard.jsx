e2eb2d5d3ca741478583f8c9cea7becfe0b97367
DESKTOP-9Q9S1KN\jayve <jayvee.salango10@gmail.com>
2025-11-12 03:47:49 +0800

    feat: heart react svg into component

--- src/components/TweetCard.jsx
+++ src/components/TweetCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import api from "../api";
 import { timeAgo } from "../utils/timeAgo";
+import Heart from "./svg/Heart";
 
 export default function TweetCard({ tweet, onLikeToggle }) {
   const [liked, setLiked] = useState(tweet.liked_by_me || false);
@@ -48,34 +49,9 @@ export default function TweetCard({ tweet, onLikeToggle }) {
               className="flex items-center cursor-pointer gap-2"
             >
               {liked ? (
-                <svg
-                  width="18"
-                  height="16"
-                  viewBox="0 0 18 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M1.31804 1.31804C0.900169 1.7359 0.568697 2.23198 0.342547 2.77795C0.116398 3.32392 0 3.90909 0 4.50004C0 5.09099 0.116398 5.67616 0.342547 6.22213C0.568697 6.7681 0.900169 7.26417 1.31804 7.68204L9.00004 15.364L16.682 7.68204C17.526 6.83812 18.0001 5.69352 18.0001 4.50004C18.0001 3.30656 17.526 2.16196 16.682 1.31804C15.8381 0.47412 14.6935 1.1453e-05 13.5 1.14441e-05C12.3066 1.14352e-05 11.162 0.47412 10.318 1.31804L9.00004 2.63604L7.68204 1.31804C7.26417 0.900169 6.7681 0.568697 6.22213 0.342547C5.67616 0.116398 5.09099 0 4.50004 0C3.90909 0 3.32392 0.116398 2.77795 0.342547C2.23198 0.568697 1.7359 0.900169 1.31804 1.31804Z"
-                    fill="#D72600"
-                  />
-                </svg>
+                <Heart width={20} height={18} strokeColor="red" fillColor="red" />
               ) : (
-                <svg
-                  width="20"
-                  height="18"
-                  viewBox="0 0 20 18"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M2.31804 2.31804C1.90017 2.7359 1.5687 3.23198 1.34255 3.77795C1.1164 4.32392 1 4.90909 1 5.50004C1 6.09099 1.1164 6.67616 1.34255 7.22213C1.5687 7.7681 1.90017 8.26417 2.31804 8.68204L10 16.364L17.682 8.68204C18.526 7.83812 19.0001 6.69352 19.0001 5.50004C19.0001 4.30656 18.526 3.16196 17.682 2.31804C16.8381 1.47412 15.6935 1.00001 14.5 1.00001C13.3066 1.00001 12.162 1.47412 11.318 2.31804L10 3.63604L8.68204 2.31804C8.26417 1.90017 7.7681 1.5687 7.22213 1.34255C6.67616 1.1164 6.09099 1 5.50004 1C4.90909 1 4.32392 1.1164 3.77795 1.34255C3.23198 1.5687 2.7359 1.90017 2.31804 2.31804Z"
-                    stroke="black"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                <Heart width={20} height={18} strokeColor="black" fillColor="none" />
               )}
 
               <span className="inter-semibold text-sm">{likesCount}</span>
